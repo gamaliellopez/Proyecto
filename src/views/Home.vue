55c95d1c9471990c377ef24854be0711d22bd238
gamaliellopez <38816960+gamaliellopez@users.noreply.github.com>
2021-02-06 20:23:42 -0600

    nuevos nuevos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,11 +24,17 @@
       </b-carousel>
     </div>
     <div class="container__p">
-        <h1>Portal mascotas </h1>
-        <p class="text-justify">
-            {{texto}}
-        </p>
+      <h1>Portal mascotas</h1>
+      <p class="text-justify">
+        {{ texto }}
+      </p>
+      <!-- <img
+        src="https://miproiecto.com/Prueba/Capturas.PNG"
+      /> -->
     </div>
+    <iframe 
+    src="https://view.officeapps.live.com/op/embed.aspx?src=https://miproiecto.com/Prueba/USUARIOS-INNDOT.xlsx"
+     width="100%" height="565px" frameborder="0"></iframe>
   </div>
 </template>
 
@@ -40,7 +46,10 @@ export default {
   },
   data() {
     return {
-      texto: "Te damos la bienvenida al portal canino, pensado para ser la página principal de aquellas personas interesadas y amantes de los perros y todo lo relacionado con estos animales.Si quieres colaborar, aquí podrás organizar tu trabajo.El perro es uno de los animales domésticos más antiguos del mundo y el mejor amigo del hombre. Su domesticación comienza cuando apenas era un pariente salvaje de los actuales lobos (Canis lupus), existiendo pruebas arqueológicas que confirman su existencia como animal doméstico. Toda la especie se refiere a la raza doméstica (Canis lupus familiaris)",
+      src:
+        "https://view.officeapps.live.com/op/embed.aspx?src=https://miproiecto.com/Prueba/USUARIOS-INNDOT.xlsx",
+      texto:
+        "Te damos la bienvenida al portal canino, pensado para ser la página principal de aquellas personas interesadas y amantes de los perros y todo lo relacionado con estos animales.Si quieres colaborar, aquí podrás organizar tu trabajo.El perro es uno de los animales domésticos más antiguos del mundo y el mejor amigo del hombre. Su domesticación comienza cuando apenas era un pariente salvaje de los actuales lobos (Canis lupus), existiendo pruebas arqueológicas que confirman su existencia como animal doméstico. Toda la especie se refiere a la raza doméstica (Canis lupus familiaris)",
     };
   },
   methods: {},
@@ -49,9 +58,9 @@ export default {
 
 <style scoped>
 .container__carousel {
-    margin-top: 10px;
+  margin-top: 10px;
 }
 .container__p {
-    margin-top: 30px;
+  margin-top: 30px;
 }
 </style>
